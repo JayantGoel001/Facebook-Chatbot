b914be4e00133044c36c7355a2b6612ac8ea75d8
username <54479676+CormacKrum@users.noreply.github.com>
2020-11-22 02:26:44 +0530

    Initial Commit

--- sender.js
+++ sender.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const local_token = "123456"
 app.get('/',(req,res)=>{
     console.log(req.query);
-    res.send("Hello World");
+    const challenge = req.query['hub.challenge'];
+    const verify_token = req.query['hub.verify_token'];
+    if (local_token===verify_token) {
+        res.send(challenge);
+    }else {
+        res.send("Hello World");
+    }
 })
 
 app.listen(port,()=>{
