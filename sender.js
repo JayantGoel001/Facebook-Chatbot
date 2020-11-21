365ae36676c9e835ea6d3d2db6219c445b3645e3
username <54479676+CormacKrum@users.noreply.github.com>
2020-11-21 18:48:06 +0530

    Initial Commit

---
+++ sender.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/',(req,res)=>{
+    console.log(req.query);
+    res.send("Hello World");
+})
+
+app.listen(port,()=>{
+    console.log(`App Listening at localhost:${port}`);
+})
